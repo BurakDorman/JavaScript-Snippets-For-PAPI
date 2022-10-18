0c1d4a647a60ca5e9f7194a7d4f4b9a3c1c90317
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2022-10-18 13:32:30 +0300

    Update player_party_name.js

--- src/player_party_name.js
+++ src/player_party_name.js
@@ -1,12 +1,13 @@
-//
+// This code checks if the user is in a clan and formats output with a space.
+// The placeholders come from UltimateClansV5 of UlrichCraft.
 var has = "%uclans_hasclan%";
 var name = "%uclans_hasclan_formated%";
 
 function formatPartyName() {
     if (has === "true") {
-        return name
+        return name + " "
     } else {
-        return "None"
+        return "None" + " "
     }
 }
 
